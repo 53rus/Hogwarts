0e26428818748420f815011bf3b16581193a85cc
53rus <53rus@mail.ru>
2024-02-14 01:17:41 +0300

    corrected work

--- src/MetodBlock.java
+++ src/MetodBlock.java
@@ -1,7 +1,9 @@
+import java.util.Arrays;
+
 public class MetodBlock {
 
 
-    public void checkHogwarts (Hogwarts[] hogwarts,String name) {
+    public void chooseStudentHogwarts(Hogwarts[] hogwarts, String name) {
         for (int i = 0; i < hogwarts.length; i++) {
             if (hogwarts[i].getName().equalsIgnoreCase(name)) {
                 System.out.println(hogwarts[i]);
@@ -9,7 +11,7 @@ public class MetodBlock {
         }
     }
 
-    public void compareStudent(Hogwarts[] hogwarts, String name, String name1) {
+    public void compareStudentHogwarts(Hogwarts[] hogwarts, String name, String name1) {
         Hogwarts maxPower = hogwarts[0];
         for (int i = 0; i < hogwarts.length; i++) {
             if (hogwarts[i].getName().equalsIgnoreCase(name)) {
@@ -18,6 +20,8 @@ public class MetodBlock {
             if (hogwarts[i].getName().equalsIgnoreCase(name1)) {
                 if (maxPower.getMagicPower() < hogwarts[i].getMagicPower()) {
                     System.out.println("Студент: " + hogwarts[i].getName() + " обладает больщей силой магии, чем студент: " + maxPower.getName() + " " + hogwarts[i].getMagicPower() + " против " + maxPower.getMagicPower());
+                } else if (maxPower.getMagicPower() == hogwarts[i].getMagicPower()) {
+                    System.out.println("Cтуденты " + hogwarts[i].getName() + " и " + maxPower.getName() + " обладают одинаковой силой магии " + hogwarts[i].getMagicPower() + " против " + maxPower.getMagicPower());
                 } else {
                     System.out.println("Студент: " + maxPower.getName() + " обладает больщей силой магии, чем студент: " + hogwarts[i].getName() + " " + maxPower.getMagicPower() + " против " + hogwarts[i].getMagicPower());
                 }
@@ -25,61 +29,123 @@ public class MetodBlock {
         }
     }
 
-    public  void bestGriffindor (Griffindor[] griffindors) {
+
+    public void compareStudentGriffindore(Hogwarts[] hogwarts, String name, String name1) {
         int sum = 0;
-        int sum1 = 0;
-        int number = 0;
-        for (int i = 0; i < griffindors.length; i++) {
-            Griffindor griffindor = griffindors[i];
-            sum1 = griffindor.getBravery() + griffindor.getHonor() + griffindor.getNobility();
-            if (sum < sum1) {
-                sum = sum1;
-                number = i;
+        int summ = 0;
+        Hogwarts griff = null;
+        Hogwarts griff2 = null;
+        for (int i = 0; i < hogwarts.length; i++) {
+            if (hogwarts[i].getName().equalsIgnoreCase(name) && hogwarts[i] instanceof Griffindor) {
+                Griffindor student = ((Griffindor) hogwarts[i]);
+                griff = student;
+                sum = student.getBravery() + student.getNobility() + student.getHonor();
+            }
+
+            if (hogwarts[i].getName().equalsIgnoreCase(name1) && hogwarts[i] instanceof Griffindor) {
+                Griffindor student = ((Griffindor) hogwarts[i]);
+                griff2 = student;
+                summ = student.getBravery() + student.getNobility() + student.getHonor();
             }
         }
-        System.out.println(griffindors[number].getName() + " Лучший в Гриффиндоре");
+
+        if (sum < summ) {
+                System.out.println("Студент: " + griff2.getName() + " обладает большим количеством баллов, чем студент : " + griff.getName() + " " + summ + " против " + sum + ". Факультет Гриффиндор");
+            } else if (sum == summ) {
+                System.out.println("Студенты: " + griff.getName() + " и " + griff2.getName() + " обладают одинаковым колличеством баллов " + sum + " и " + summ + " соответственно. Факультет Гриффиндор");
+            } else {
+                System.out.println("Студент: " + griff.getName() + " обладает большим количеством баллов, чем студент : " + griff2.getName() + " " + sum + " против " + summ + ". Факультет Гриффиндор");
+        }
     }
-    public  void bestPuffenduy(Puffenduy[] puffenduys) {
+
+    public void compareStudentPuffenduy(Hogwarts[] hogwarts, String name, String name1) {
         int sum = 0;
-        int sum1 = 0;
-        int number = 0;
-        for (int i = 0; i < puffenduys.length; i++) {
-            Puffenduy puffenduy = puffenduys[i];
-            sum1 = puffenduy.getHardWork() + puffenduy.getHonesty() + puffenduy.getLoyalty();
-            if (sum < sum1) {
-                sum = sum1;
-                number = i;
+        int summ = 0;
+        Hogwarts puff = null;
+        Hogwarts puff2 = null;
+        for (int i = 0; i < hogwarts.length; i++) {
+            if (hogwarts[i].getName().equalsIgnoreCase(name) && hogwarts[i] instanceof Puffenduy) {
+                Puffenduy student = ((Puffenduy) hogwarts[i]);
+                puff = student;
+                sum = student.getHardWork() + student.getHonesty() + student.getLoyalty();
+            }
+
+            if (hogwarts[i].getName().equalsIgnoreCase(name1) && hogwarts[i] instanceof Puffenduy) {
+                Puffenduy student = ((Puffenduy) hogwarts[i]);
+                puff2 = student;
+                summ = student.getHardWork() + student.getHonesty() + student.getLoyalty();
             }
         }
-        System.out.println(puffenduys[number].getName() + " Лучший в Пуффендуе ");
+
+        if (sum < summ) {
+            System.out.println("Студент: " + puff2.getName() + " обладает большим количеством баллов, чем студент : " + puff.getName() + " " + summ + " против " + sum  + ". Факультет Пуффендуй");
+        } else if (sum == summ) {
+            System.out.println("Студенты: " + puff.getName() + " и " + puff2.getName() + " обладают одинаковым колличеством баллов " + sum + " и " + summ + " соответственно. Факультет Пуффендуй");
+        } else {
+            System.out.println("Студент: " + puff.getName() + " обладает большим количеством баллов, чем студент : " + puff2.getName() + " " + sum + " против " + summ + ". Факультет Пуффендуй");
+        }
     }
-    public  void bestKogtevran(Kogtevran[] kogtevrans) {
+
+    public void compareStudentSlizerin(Hogwarts[] hogwarts, String name, String name1) {
         int sum = 0;
-        int sum1 = 0;
-        int number = 0;
-        for (int i = 0; i < kogtevrans.length; i++) {
-            Kogtevran kogtevran = kogtevrans[i];
-            sum1 = kogtevran.getCreativity() + kogtevran.getWit() + kogtevran.getWisdom() + kogtevran.getMind();
-            if (sum < sum1) {
-                sum = sum1;
-                number = i;
+        int summ = 0;
+        Hogwarts sliz = null;
+        Hogwarts sliz2 = null;
+        for (int i = 0; i < hogwarts.length; i++) {
+            if (hogwarts[i].getName().equalsIgnoreCase(name) && hogwarts[i] instanceof Slizerin) {
+                Slizerin student = ((Slizerin) hogwarts[i]);
+                sliz = student;
+                sum = student.getAmbition() + student.getCunning() + student.getDetermination() + student.getLustForPower() + student.getResourcefulness();
+            }
+
+            if (hogwarts[i].getName().equalsIgnoreCase(name1) && hogwarts[i] instanceof Slizerin) {
+                Slizerin student = ((Slizerin) hogwarts[i]);
+                sliz2 = student;
+                summ = student.getAmbition() + student.getCunning() + student.getDetermination() + student.getLustForPower() + student.getResourcefulness();
             }
         }
-        System.out.println(kogtevrans[number].getName() + " Лучший в Когтевранце ");
+
+        if (sum < summ) {
+            System.out.println("Студент: " + sliz2.getName() + " обладает большим количеством баллов, чем студент : " + sliz.getName() + " " + summ + " против " + sum  + ". Факультет Слизерин");
+        } else if (sum == summ) {
+            System.out.println("Студенты: " + sliz.getName() + " и " + sliz2.getName() + " обладают одинаковым колличеством баллов " + sum + " и " + summ + " соответственно. Факультет Слизерин");
+        } else {
+            System.out.println("Студент: " + sliz.getName() + " обладает большим количеством баллов, чем студент : " + sliz2.getName() + " " + sum + " против " + summ + ". Факультет Слизерин");
+        }
     }
-    public  void bestSlizerin(Slizerin[] slizerins) {
+
+    public void compareStudentKogtevran(Hogwarts[] hogwarts, String name, String name1) {
         int sum = 0;
-        int sum1 = 0;
-        int number = 0;
-        for (int i = 0; i < slizerins.length; i++) {
-            Slizerin slizerin = slizerins[i];
-            sum1 = slizerin.getCunning() + slizerin.getDetermination() + slizerin.getAmbition() + slizerin.getResourcefulness() + slizerin.getLustForPower();
-            if (sum < sum1) {
-                sum = sum1;
-                number = i;
+        int summ = 0;
+        Hogwarts kogt = null;
+        Hogwarts kogt2 = null;
+        for (int i = 0; i < hogwarts.length; i++) {
+            if (hogwarts[i].getName().equalsIgnoreCase(name) && hogwarts[i] instanceof Kogtevran) {
+                Kogtevran student = ((Kogtevran) hogwarts[i]);
+                kogt = student;
+                sum = student.getCreativity() + student.getMind() + student.getWit() + student.getWisdom();
+            }
+
+            if (hogwarts[i].getName().equalsIgnoreCase(name1) && hogwarts[i] instanceof Kogtevran) {
+                Kogtevran student = ((Kogtevran) hogwarts[i]);
+                kogt2 = student;
+                summ = student.getCreativity() + student.getMind() + student.getWit() + student.getWisdom();
             }
         }
-        System.out.println(slizerins[number].getName() + " Лучший в Слизерин ");
+
+        if (sum < summ) {
+            System.out.println("Студент: " + kogt2.getName() + " обладает большим количеством баллов, чем студент : " + kogt.getName() + " " + summ + " против " + sum  + ". Факультет Когтевран");
+        } else if (sum == summ) {
+            System.out.println("Студенты: " + kogt.getName() + " и " + kogt2.getName() + " обладают одинаковым колличеством баллов " + sum + " и " + summ + " соответственно. Факультет Когтевран");
+        } else {
+            System.out.println("Студент: " + kogt.getName() + " обладает большим количеством баллов, чем студент : " + kogt2.getName() + " " + sum + " против " + summ + ". Факультет Когтевран");
+        }
     }
+
+
+
+
+
+
 
 }
